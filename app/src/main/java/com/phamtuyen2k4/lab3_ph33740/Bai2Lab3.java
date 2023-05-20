5b0bb03d28a3e22b2b91c2c0f90b74fb059123ef
Duy Le <lehongduy.bk@gmail.com>
2023-05-20 17:21:34 +0700

    Initial commit

---
+++ app/src/main/java/com/phamtuyen2k4/lab3_ph33740/Bai2Lab3.java
@@ -0,0 +1,14 @@
+package com.phamtuyen2k4.lab3_ph33740;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Bai2Lab3 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bai2_lab3);
+    }
+}
---
+++ app/src/main/java/com/phamtuyen2k4/lab3_ph33740/MainActivity.java
@@ -0,0 +1,14 @@
+package com.phamtuyen2k4.lab3_ph33740;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bai3_lab3);
+    }
+}
